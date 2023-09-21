1cde0749ac27c04a0d6233c6b0e002b8088d097d
Jaan Tollander de Balsch <jtollan@dalripa.lnx.csc.fi>
2023-09-21 10:52:47 +0300

    notes

---
+++ examples/singleton-pattern.jl
@@ -0,0 +1,22 @@
+# Singleton is struct without fields
+struct Singleton end
+
+# We can use singleton for dispatch
+f(::Type{Singleton}) = "hello"
+f(Singleton)
+
+# We can use a singleton as a value
+const singleton = Singleton()
+f(::Singleton) = "singleton"
+f(singleton)
+
+# We can use singletons for multiple dispatch
+abstract type S end
+struct S1 <: S end
+struct S2 <: S end
+
+f(::Type{S1}) = "dispatch S1"
+f(::Type{S2}) = "dispatch S2"
+
+f(S1)
+f(S2)
