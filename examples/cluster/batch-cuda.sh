53c5168e364f496c8452c947ef1243af5effebca
Jaan Tollander de Balsch <jtollan@dalripa.lnx.csc.fi>
2023-09-22 12:17:10 +0300

    fix cuda

--- examples/cluster/batch-cuda.sh
+++ examples/cluster/batch-cuda.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#SBATCH --partition=small
+#SBATCH --partition=gpu
 #SBATCH --time=00:15:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=3
-#SBATCH --mem-per-cpu=1000
+#SBATCH --cpus-per-task=10
+#SBATCH --gres=gpu:v100:1
+#SBATCH --mem-per-cpu=8000
 
 module load julia/1.9
 julia --project=. -e 'using Pkg; Pkg.instantiate()'
