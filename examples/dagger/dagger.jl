f337f11ad20f350730263be0e1fa9657954c4537
Jaan Tollander de Balsch <jtollan@dalripa.lnx.csc.fi>
2023-09-22 12:02:21 +0300

    notes

--- examples/dagger/dagger.jl
+++ examples/dagger/dagger.jl
@@ -2,7 +2,7 @@ using Distributed
 addprocs(2; exeflags="--threads=2")
 
 @everywhere function task()
-    return (getpid(), Threads.threadid())
+    return (myid(), Threads.threadid())
 end
 
 using Dagger
@@ -10,18 +10,16 @@ t = [Dagger.@spawn task() for _ in 1:10]
 x = fetch.(t)
 
 # We can see that Dagger will run the tasks on available workers and threads
-println("Main process run with pid $(getpid()) on thread 1.")
+println("worker id | thread id")
+println("Main process:")
+println(task())
 println("Dagger processes:")
-println("pid | threadid")
-for (pid, tid) in unique(x)
-    println("$pid | $tid")
-end
+println.(unique(x))
 
 # Directed acyclic graph
-a = Dagger.@spawn rand(100, 100)
-b = Dagger.@spawn sum(a)
-c = Dagger.@spawn prod(a)
-d = Dagger.@spawn b+c
-println(fetch(d))
+a = Dagger.spawn(rand, 100, 100)
+b = [Dagger.spawn(x -> sum(n.*x), a) for n in 1:5]
+c = Dagger.spawn((x...) -> sum(x), b...)
+println(fetch(c))
 
-rmprocs(workers())
+#rmprocs(workers())
